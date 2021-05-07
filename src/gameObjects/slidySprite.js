53cc289b6a2c90bf7191c80e74764b070406dd61
TPlemur <TPlemur@gmail.com>
2021-05-07 12:37:12 -0700

    implemented slidySprite

---
+++ src/gameObjects/slidySprite.js
@@ -0,0 +1,29 @@
+//SlidySprite.js
+//creates a sprite object with attached tween functions for sliding and rotation
+
+class SlidySprite extends Phaser.GameObject.Sprite {
+    constructor(scene,x,y,texture,frame){
+    super(scene,x,y,texture,frame)
+    }
+
+    //tween from current position to x,y over time (in miliseconds)
+    slide(x,y,time, ease = 'liner'){
+        this.scene.tweens.add({
+            targets: this,
+            x: x,
+            y: y,
+            ease:ease,
+            duration: time
+        })
+    }
+
+    //tween from current angle to angle over time(in miliseconds)
+    rotate(angle,time,ease = 'liner'){
+        this.scene.tweens.add({
+            targets: this,
+            angle: angle,
+            ease: ease,
+            duration: time
+        })
+    }  
+}
