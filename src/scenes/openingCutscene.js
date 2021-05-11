dd0be8a09ea2f7a47c5dd6b76691633ae7ffdbdd
TPlemur <TPlemur@gmail.com>
2021-05-10 19:51:59 -0700

    finalized right to left screen wipe moved screenwipe to main

--- src/scenes/openingCutscene.js
+++ src/scenes/openingCutscene.js
@@ -13,24 +13,8 @@ class Opening extends Phaser.Scene {
 
         //debugging mode features
         debugCreate(this);
+        RLWipe(this,3000,'wipeTree')
 
-        this.wipetree = new SlidySprite(this,screenWidth,0,'wipeTree').setOrigin(0.5,0);
-        this.add.existing(this.wipetree);
-        this.physics.add.existing(this.wipetree);
-        this.wipebox = this.add.rectangle(screenWidth,0,screenWidth,screenHeight,0x000000).setOrigin(0)
-        this.wipetime = 8000
-        this.wipeBuffer =25
-        this.wipetree.slide(0,0,this.wipetime/2)
-        this.tweens.add({
-            targets: this.wipebox,
-            x: -screenWidth,
-            ease: 'Linear',
-            duration: this.wipetime
-        })
-        this.call = this.time.delayedCall(this.wipetime/2 + this.wipeBuffer,() =>{
-            this.wipetree.x = screenWidth;
-            this.wipetree.slide(0,0,this.wipetime/2);
-        });
         
     }
     update(){
@@ -39,3 +23,4 @@ class Opening extends Phaser.Scene {
     }
 
 }
+
