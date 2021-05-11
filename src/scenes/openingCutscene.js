892a882b834fef97c9891985fe485feed343250b
TPlemur <TPlemur@gmail.com>
2021-05-10 19:08:01 -0700

    prototype screen change

--- src/scenes/openingCutscene.js
+++ src/scenes/openingCutscene.js
@@ -14,13 +14,24 @@ class Opening extends Phaser.Scene {
         //debugging mode features
         debugCreate(this);
 
-        this.wipetree = new SlidySprite(this,screenWidth,screenHeight,'wipeTree').setOrigin(0.5,1);
+        this.wipetree = new SlidySprite(this,screenWidth,0,'wipeTree').setOrigin(0.5,0);
         this.add.existing(this.wipetree);
         this.physics.add.existing(this.wipetree);
-
-
-
-
+        this.wipebox = this.add.rectangle(screenWidth,0,screenWidth,screenHeight,0x000000).setOrigin(0)
+        this.wipetime = 8000
+        this.wipeBuffer =25
+        this.wipetree.slide(0,0,this.wipetime/2)
+        this.tweens.add({
+            targets: this.wipebox,
+            x: -screenWidth,
+            ease: 'Linear',
+            duration: this.wipetime
+        })
+        this.call = this.time.delayedCall(this.wipetime/2 + this.wipeBuffer,() =>{
+            this.wipetree.x = screenWidth;
+            this.wipetree.slide(0,0,this.wipetime/2);
+        });
+        
     }
     update(){
         debugUpdate(this)
