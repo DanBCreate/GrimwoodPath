8c7f3ada7fb99790f6e457d4e6cb5e5c86620363
Danny Baghdasarians <dbaghdas@ucsc.edu>
2021-05-11 18:24:46 -0700

    Pulling slidy blocks works, pushing still needs work

--- src/gameObjects/slidyBlock.js
+++ src/gameObjects/slidyBlock.js
@@ -2,12 +2,50 @@ class slidyBlock extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x , y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.physics.add.existing(this);
-        scene.add.existing(this);    
+        scene.add.existing(this);   
+        
+        this.dropped = false;
+
+        this.movable = false;
+        this.playerX = 0;
     }
 
-    create(){}
+    create(){
+        // Used for initially spawning the block
+        this.block.body.allowGravity = true; 
+        this.setVelocityX(0);
 
-    update(){
+    }
+
+    update(){    
+        // Moves the block with the player
+        if(this.movable == true){
+            //this.setImmovable(false);
+            this.setVelocityX(this.playerX);
+        }
+        else{
+            this.setVelocityX(0);
+        }
+
+        // Collides with world bounds
         this.setCollideWorldBounds(true); 
+    }
+
+    checkProximity(playerX){
+        if(Math.abs(playerX - this.x) < 100){
+            return true;
+        }
+    }
+
+    setMovable(movable, player){
+
+        if(movable == true){
+            this.playerX = player;
+            this.movable = true;
+        }
+        else{
+            this.playerX = 0;
+            this.movable = false;
+        }
     }
 }
