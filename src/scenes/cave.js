4fe574e551be0c282a364bbd8725907f5b41f158
TPlemur <TPlemur@gmail.com>
2021-05-14 02:23:45 -0700

    Fade in cave scene

--- src/scenes/cave.js
+++ src/scenes/cave.js
@@ -6,7 +6,13 @@ class Cave extends Phaser.Scene {
     }
 
     preload(){
-
+        this.screentint =this.add.rectangle(screenWidth,screenHeight,screenWidth,screenHeight,0x000000).setOrigin(1)
+        this.tweens.add({
+            targets: this.screentint,
+            alpha: 0,
+            duration: 10000,     
+            ease: 'linear'               
+        })
     }
     create(){
 
