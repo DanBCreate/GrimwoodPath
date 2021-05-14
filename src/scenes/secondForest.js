59d53167484fed065399231a9b7792933a1965e3
TPlemur <TPlemur@gmail.com>
2021-05-14 12:27:37 -0700

    added player and ground to secondForest

--- src/scenes/secondForest.js
+++ src/scenes/secondForest.js
@@ -11,7 +11,22 @@ class LForest extends Phaser.Scene {
     create(){
 
         //debugging mode features
-        debugCreate(this);     
+        debugCreate(this);    
+        
+        //set up the ground
+        this.ground = this.physics.add.sprite(screenCenterX, screenHeight, 'ground').setScale(0.05); // Initialize our ground
+        this.ground.setImmovable(true); // Sets ground to immovable
+        this.ground.body.allowGravity = false; // So gravity has no effect ground
+        this.ground.displayWidth = screenWidth;
+        this.ground.setOrigin(0.5,1)       
+
+        // Setting up our player and camera to follow player
+        this.player = new player(this, screenCenterX, screenCenterY, 'player').setScale(0.05); // Initialize our Player
+        this.sceneCamera = this.cameras.main.startFollow(this.player);
+        this.sceneCamera.setLerp(cameraLerp,cameraLerp)
+
+        //collide with the ground
+        this.physics.add.collider(this.player, this.ground);
 
 
 
@@ -19,6 +34,7 @@ class LForest extends Phaser.Scene {
     update(){
         //debugging mode features
         debugUpdate(this);
+        this.player.update();
 
     }
 
