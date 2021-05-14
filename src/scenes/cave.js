9ca2eb6b96d91dbbc50ce3b76a516adc10cee1ff
TPlemur <TPlemur@gmail.com>
2021-05-14 12:27:54 -0700

    added player and gound to Cave

--- src/scenes/cave.js
+++ src/scenes/cave.js
@@ -10,7 +10,7 @@ class Cave extends Phaser.Scene {
         this.tweens.add({
             targets: this.screentint,
             alpha: 0,
-            duration: 10000,     
+            duration: 1000,     
             ease: 'linear'               
         })
     }
@@ -19,11 +19,29 @@ class Cave extends Phaser.Scene {
         //debugging mode features
         debugCreate(this); 
 
+        //set up the ground
+        this.ground = this.physics.add.sprite(screenCenterX, screenHeight, 'ground').setScale(0.05); // Initialize our ground
+        this.ground.setImmovable(true); // Sets ground to immovable
+        this.ground.body.allowGravity = false; // So gravity has no effect ground
+        this.ground.displayWidth = screenWidth;
+        this.ground.setOrigin(0.5,1)       
+
+        // Setting up our player and camera to follow player
+        this.player = new player(this, screenCenterX, screenCenterY, 'player').setScale(0.05); // Initialize our Player
+        this.sceneCamera = this.cameras.main.startFollow(this.player);
+        this.sceneCamera.setLerp(cameraLerp,cameraLerp)
+
+        //collide with the ground
+        this.physics.add.collider(this.player, this.ground);
+
+
+
 
 
     }
     update(){
         debugUpdate(this);
+        this.player.update()
 
     }
 
